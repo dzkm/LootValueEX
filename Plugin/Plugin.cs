8f55a82510f259ba3a1959cd159befe6d17c36b7
Djonathan de Souza <13489382+dzkm@users.noreply.github.com>
2024-09-02 20:32:59 -0300

    chore: initial commit

---
+++ Plugin/Plugin.cs
@@ -0,0 +1,15 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+
+namespace LootValueEX
+{
+    [BepInPlugin("pro.kaiden.lootvalueex", "LootValueEX", "0.2.0")]
+    internal class Plugin : BaseUnityPlugin
+    {
+        internal static ManualLogSource Log;
+        private void Awake()
+        {
+            Log = base.Logger;
+        }
+    }
+}
